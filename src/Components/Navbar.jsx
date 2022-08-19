58d39b00e0318fc589f49ba4d720f7838be7e138
Mauro.Boto <irinayadolfito@gmail.com>
2022-08-19 20:35:16 -0300

    Segundo desafio

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,45 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return(
+        <section>
+            <nav>
+                <ul id="menu">
+                    <li><a href="../PERFUMERIA  BOTTONE/Index.html">HOME</a></li>
+                    <li>
+                        <a href="../PERFUMERIA  BOTTONE/Pages/nosotros.html">Nosotros</a>
+                    </li>
+                    <li>
+                        <a href="../PERFUMERIA  BOTTONE/Pages/catalogo.html">Catalogo</a>
+                    </li>
+                    <li>
+                        <a href="../PERFUMERIA  BOTTONE/Pages/productos.html">Productos</a>
+                    </li>
+                    <li>
+                        <a href="../PERFUMERIA  BOTTONE/Pages/contacto.html">Contacto</a>
+                        <ul>
+                            <h2>Nuestras redes</h2>
+                            <li>
+                                <a href="https://es-la.facebook.com/">Facebook</a>
+                            </li>
+                            <li>
+                                <a  href="https://instagram.com/">Instagram</a>
+                            </li>
+                            <li>
+                                <a  href="https://twitter.com/">Twitter</a>
+                            </li>
+                            <h2>Nuestros telefonos</h2>
+                            <li>011-1234-5678</li>
+                            <li>
+                                <a href="mailto:PerfumeriaMiri@gmail.com.ar">Mandanos un E-mail</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+    </section>
+    )
+};
+
+
+export default Navbar;
